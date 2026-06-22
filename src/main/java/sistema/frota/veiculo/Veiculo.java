68f8ba9047f72375ca2c9debcb74728ab8d7cf58
yumi <ligiayumimeira@gmail.com>
2026-06-22 09:54:30 -0300

    feat: adiciona colunas veiculo

--- src/main/java/sistema/frota/veiculo/Veiculo.java
+++ src/main/java/sistema/frota/veiculo/Veiculo.java
@@ -3,13 +3,44 @@ package sistema.frota.veiculo;
 import jakarta.persistence.*;
 import lombok.*;
 
-@Table(name = "produtos")
-@Entity(name = "produtos")
+import java.time.Year;
+
+@Table(name = "veiculo")
+@Entity
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@EqualsAndHashCode(of = "id")
+@EqualsAndHashCode(of = "chassi")
 
 public class Veiculo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(columnDefinition = "VARCHAR(17)")
+    @NonNull
+    private String chassi;
+
+    @Column(columnDefinition = "VARCHAR(11)")
+    @NonNull
+    private String renavam;
+
+    @Column(columnDefinition = "VARCHAR(7)")
+    @NonNull
+    private String placa;
+
+    @NonNull
+    private String marca;
+
+    @NonNull
+    private String modelo;
+
+    @NonNull
+    private Year ano;
+
+    @NonNull
+    private int km_atual;
+
+    @NonNull
+    @Column(precision = 10, scale = 2)
+    private double capacidade_carga;
 }
