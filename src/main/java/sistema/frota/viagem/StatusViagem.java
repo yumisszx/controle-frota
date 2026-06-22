d34a13f79f1dbc968bc5ce9274905087e3c39b04
yumi <ligiayumimeira@gmail.com>
2026-06-22 11:10:11 -0300

    feat: new class viagem

---
+++ src/main/java/sistema/frota/viagem/StatusViagem.java
@@ -0,0 +1,6 @@
+package sistema.frota.viagem;
+
+public enum StatusViagem {
+    EM_ANDAMENTO,
+    FINALIZADA
+}
---
+++ src/main/java/sistema/frota/viagem/Viagem.java
@@ -0,0 +1,62 @@
+package sistema.frota.viagem;
+
+import jakarta.persistence.*;
+import lombok.*;
+import sistema.frota.motorista.Motorista;
+import sistema.frota.veiculo.Veiculo;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Table(name = "viagem")
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id_viagem")
+public class Viagem {
+    @Id
+    @NonNull
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id_viagem;
+
+    @NonNull
+    @ManyToOne
+    @JoinColumn(name = "veiculo_chassi")
+    private Veiculo veiculo;
+
+    @NonNull
+    @ManyToOne
+    @JoinColumn(name = "motorista_cpf")
+    private Motorista motorista;
+
+    @NonNull
+    @ManyToOne
+    @JoinColumn(name = "origem_id")
+    private Cidades origem;
+
+    @NonNull
+    @ManyToOne
+    @JoinColumn(name = "destino_id")
+    private Cidades destino;
+
+    @NonNull
+    private LocalDate data_saida;
+
+    private LocalDate data_chagada;
+
+    @NonNull
+    private LocalTime hora_saida;
+
+    private LocalTime hora_chagada;
+
+    @NonNull
+    private Integer km_inicial;
+
+    private Integer km_final;
+
+    @NonNull
+    @Enumerated(EnumType.STRING)
+    private StatusViagem status;
+}
