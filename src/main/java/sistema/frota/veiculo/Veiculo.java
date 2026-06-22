dbb24e8c0c042da4a350a984c25c98dc104f02c7
yumi <ligiayumimeira@gmail.com>
2026-06-22 09:28:25 -0300

    feat: adiciona classes

---
+++ src/main/java/sistema/frota/veiculo/Veiculo.java
@@ -0,0 +1,15 @@
+package sistema.frota.veiculo;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Table(name = "produtos")
+@Entity(name = "produtos")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+
+public class Veiculo {
+}
